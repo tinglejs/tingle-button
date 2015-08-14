be8c4b6c912c1194d562df6036711bb28d717bef
yize <hanchao.shc@alibaba-inc.com>
2015-08-14 15:53:34 +0800

    format code

--- src/Button.js
+++ src/Button.js
@@ -25,18 +25,18 @@ class Button extends React.Component {
         let text = type === 'text';
         let disabled = !!this.props.disabled;
         let classSet = {
-                [this.props.className]: !!this.props.className,
-                'tButton tFAC': true,
-                'tFS12 tPL16 tPR16': small,
-                'tFS14': medium,
-                'tFS18 tH44 tR5 tLH44': large,
-                'tH30 tR4 tLH30': small || medium,
-                'tBCc tFC9': disabled,
-                'tButtonText': text,
-                'tButtonPrimary tFCf': primary && !disabled,
-                'tButtonSecondary tFC3': secondary && !disabled
-            };
-        
+            [this.props.className]: !!this.props.className,
+            'tButton tFAC': true,
+            'tFS12 tPL16 tPR16': small,
+            'tFS14': medium,
+            'tFS18 tH44 tR5 tLH44': large,
+            'tH30 tR4 tLH30': small || medium,
+            'tBCc tFC9': disabled,
+            'tButtonText': text,
+            'tButtonPrimary tFCf': primary && !disabled,
+            'tButtonSecondary tFC3': secondary && !disabled
+        };
+
         return (
             <div className={classnames(classSet)} disabled={disabled} onClick={this.handleClick.bind(this)}>{this.props.children}</div>
         );
@@ -47,7 +47,8 @@ Button.defaultProps = {
     size: 'l',
     type: 'primary',
     disabled: false,
-    onClick() {}
+    onClick() {
+    }
 };
 
 // http://facebook.github.io/react/docs/reusable-components.html
